310bf33ff792b288aa49fea4323f3dbc384605d3
David Heizer <DavidHeizer@QuickenLoans.com>
2020-09-21 16:15:45 -0400

    MVP

---
+++ PlayingCardsAPI/Controllers/Cards.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PlayingCardsAPI.Models;
+
+namespace PlayingCardsAPI.Controllers
+{
+    public class Cards : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> NewDeck(int howManyDecks)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://deckofcardsapi.com");
+            var response = await client.GetAsync($"/api/deck/new/shuffle/?deck_count=1");
+            Deck playingDeck = await response.Content.ReadAsAsync<Deck>();
+            return View(playingDeck);
+        }
+
+        public async Task<IActionResult> GetHand(string deckid)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://deckofcardsapi.com");
+            var response = await client.GetAsync($"/api/deck/{deckid}/draw/?count=6");
+            Deck playingDeck = await response.Content.ReadAsAsync<Deck>();
+            return View(playingDeck);
+
+        }
+
+
+
+
+    }
+}
---
+++ PlayingCardsAPI/Models/Deck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayingCardsAPI.Models
+{
+    public class Deck
+    {
+        public Card[] cards { get; set; }
+        public string deck_id { get; set; }
+        public bool shuffled { get; set; }
+        public int remaining { get; set; }
+        
+
+
+    }
+}
